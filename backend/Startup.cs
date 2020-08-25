52833f7f1550144fb0ddba904ddda0f3a605835e
Cffuu <1289079621@qq.com>
2020-08-25 23:56:29 +0800

    update Startup.cs

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -36,14 +36,15 @@ namespace SyaApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(cfg =>
-            {
-                cfg.AllowAnyOrigin(); //对应跨域请求的地址
-                cfg.AllowAnyMethod(); //对应请求方法的Method
-                cfg.AllowAnyHeader(); //对应请求方法的Headers
-                                      //cfg.AllowCredentials(); //对应请求的withCredentials 值
-            });
-            
+            //app.UseCors(cfg =>
+            //{
+            //    cfg.AllowAnyOrigin(); //对应跨域请求的地址
+            //    cfg.AllowAnyMethod(); //对应请求方法的Method
+            //    cfg.AllowAnyHeader(); //对应请求方法的Headers
+            //                          //cfg.AllowCredentials(); //对应请求的withCredentials 值
+            //});
+            app.UseCors(x => x.SetIsOriginAllowed(_ => true).AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
