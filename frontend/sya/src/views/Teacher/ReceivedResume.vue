f29ff6dceab6195eab1ab7b0060b6a3680337f09
Cffuu <1289079621@qq.com>
2020-08-27 00:23:50 +0800

    update GetResumeInfo Get to Post  && ReceivedResume

--- frontend/sya/src/views/Teacher/ReceivedResume.vue
+++ frontend/sya/src/views/Teacher/ReceivedResume.vue
@@ -10,20 +10,18 @@
     </el-breadcrumb>-->
 
     <el-card>
-      <template>
-        <el-table :data="tableData" style="width: 100%">
-          <el-table-column label="#" type="index" width="180px"></el-table-column>
-          <el-table-column prop="work_name" label="工作名称" width="280px"></el-table-column>
-          <el-table-column prop="student_name" label="学生姓名" width="280px"></el-table-column>
-          <el-table-column prop="edit" label="状态" width="280px">
-            <template slot-scope="scope">
-              <!-- <el-tooltip effect="dark" content="查看" placement="top"> -->
-              <el-button type="text" size="mini" @click="getMoreInfo(scope.row)">查看详情</el-button>
-              <!-- </el-tooltip> -->
-            </template>
-          </el-table-column>
-        </el-table>
-      </template>
+      <el-table :data="tableData" style="width: 100%" v-loading="loading">
+        <el-table-column label="#" type="index" width="180px"></el-table-column>
+        <el-table-column prop="work_name" label="工作名称" width="280px"></el-table-column>
+        <el-table-column prop="student_name" label="学生姓名" width="280px"></el-table-column>
+        <el-table-column prop="edit" label="状态" width="280px">
+          <template slot-scope="scope">
+            <!-- <el-tooltip effect="dark" content="查看" placement="top"> -->
+            <el-button type="text" size="mini" @click="getMoreInfo(scope.row)">查看详情</el-button>
+            <!-- </el-tooltip> -->
+          </template>
+        </el-table-column>
+      </el-table>
       <!-- <template>
         <el-table :data="tableData" style="width: 100%">
           <el-table-column type="expand">
@@ -62,94 +60,165 @@
 
     <el-dialog title="详情" :visible.sync="checkDialogVisible" width="30%" center>
       <!-- <span>这是一段信息</span> -->
-      <el-form status-icon label-width="100px">
-        <el-form-item label="姓名" prop>
-          <el-input disabled style="width:300px;height:500px">范德萨打发但是幅度萨芬的撒阿飞的萨芬的萨芬的撒f'd's</el-input>
+      <el-form status-icon label-width="100px" :model="reSumeData">
+        <el-form-item label="姓名">
+          <el-input disabled style="width:300px" v-model="reSumeData.student_name"></el-input>
         </el-form-item>
-        <el-form-item label="城市" prop>
-          <el-input disabled style="width:300px"></el-input>
+        <el-form-item label="年龄">
+          <el-input disabled style="width:300px" v-model="reSumeData.age"></el-input>
         </el-form-item>
-        <el-form-item label="学历" prop>
-          <el-input disabled style="width:300px"></el-input>
+        <el-form-item label="城市">
+          <el-input disabled style="width:300px" v-model="reSumeData.city"></el-input>
         </el-form-item>
-        <el-form-item label="简介" prop>
-          <el-input disabled style="width:300px"></el-input>
+        <el-form-item label="学历">
+          <el-input disabled style="width:300px" v-model="reSumeData.education"></el-input>
+        </el-form-item>
+        <el-form-item label="community">
+          <el-input disabled style="width:300px" v-model="reSumeData.community"></el-input>
+        </el-form-item>
+        <el-form-item label="project">
+          <el-input disabled style="width:300px" v-model="reSumeData.project"></el-input>
+        </el-form-item>
+        <el-form-item label="academic">
+          <el-input disabled style="width:300px" v-model="reSumeData.academic"></el-input>
+        </el-form-item>
+        <el-form-item label="skill">
+          <el-input disabled style="width:300px" v-model="reSumeData.skill"></el-input>
+        </el-form-item>
+        <el-form-item label="简介">
+          <el-input disabled style="width:300px" v-model="reSumeData.introduction"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="checkDialogVisible = false">确 定</el-button>
-        <el-button @click="checkDialogVisible = false">取 消</el-button>
+        <el-tag type="success" v-if="reSumeData.status==1">已同意</el-tag>
+        <el-tag type="danger" v-if="reSumeData.status==2">已拒绝</el-tag>
+        <el-button type="primary" @click="accept()" v-if="!reSumeData.status">同 意</el-button>
+        <el-button @click="refuse()" v-if="!reSumeData.status">拒绝</el-button>
       </span>
     </el-dialog>
-
-    <!-- <el-button type="primary" @click="login()">登录</el-button> -->
   </div>
 </template>
 
 
 <script>
 import axios from "axios";
-// axios.defaults.headers.get["Content-Type"] = "application/json;charset=UTF-8";
+import { thistle } from "color-name";
 export default {
   data() {
     return {
       tableData: [],
-      checkDialogVisible: false
+      checkDialogVisible: false,
+      reSumeData: {},
+      loading: true,
     };
   },
   methods: {
+    //接收申请
+    async accept() {
+      this.checkDialogVisible = false;
+      const result = await axios.post(
+        "http://localhost:5000/Apply/ProManageApp",
+        {
+          apply_id: this.reSumeData.apply_id,
+          status: 1//同意
+        },
+        {
+          withCredentials: true
+        }
+      );
+      console.log(result);
+      // console.log(this.reSumeData);
+
+      //刷新
+      this.loading=true;
+      await this.getReceivedResume();
+    },
+    //拒绝申请
+    async refuse() {
+      this.checkDialogVisible = false;
+      this.checkDialogVisible = false;
+      const result = await axios.post(
+        "http://localhost:5000/Apply/ProManageApp",
+        {
+          apply_id: this.reSumeData.apply_id,
+          status: 2//拒绝
+        },
+        {
+          withCredentials: true
+        }
+      );
+      console.log(result);
+      // console.log(this.reSumeData);
+
+      //刷新
+      this.loading=true;
+      await this.getReceivedResume();
+    },
+    //查看简历详情
     getMoreInfo(row) {
-      console.log(row.name);
-      this.editForm = row;
+      console.log(row);
+      this.reSumeData = row;
       this.checkDialogVisible = true;
     },
-    editClickTrue() {
-      this.checkDialogVisible = false;
-      this.$message.success("成功");
-    },
-
-    // async login() {
-    //   const result = await axios.post(
-    //     "http://localhost:27682/Account/Login",
-    //     {
-    //       username: "tn@qq.com",
-    //       Password:
-    //         "ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413"
-    //     },
-    //     { withCredentials: true }
-    //   );
-    //   console.log(result);
-    // },
-
+    //获得简历信息
     async getResume(resumeId) {
       const result = await axios.post(
-        "http://localhost:27682/Resume/GetResumeInfo",
+        "http://localhost:5000/Resume/GetResumeInfo",
         {
-          resume_id: 51
+          resume_id: resumeId
+        },
+        {
+          withCredentials: true
         }
       );
       return result;
     },
 
+    //resume
+    // {
+    //     "student_id": 3,
+    //     "age": 1,
+    //     "student_name": "lzy",
+    //     "city": "shanghai",
+    //     "education": "none",
+    //     "community": "none",
+    //     "project": "none",
+    //     "academic": "none",
+    //     "skill": "none",
+    //     "introduction": "none"
+    // }
+
+    //获得申请信息
     async getReceivedResume() {
       const result = await axios.get(
-        "http://localhost:27682/Apply/ProViewApps",
+        "http://localhost:5000/Apply/ProViewApps",
         {
           withCredentials: true
         }
       );
       this.tableData = result.data.applist;
       for (var i = 0; i < this.tableData.length; i++) {
-        console.log(this.tableData[i].resume_id);
+        // console.log(this.tableData[i].resume_id);
         const resume = await this.getResume(this.tableData[i].resume_id);
+        this.tableData[i]["student_name"] = resume.data.student_name;
+        this.tableData[i]["age"] = resume.data.age;
         this.tableData[i]["city"] = resume.data.city;
-        console.log(this.tableData[i]);
+        this.tableData[i]["education"] = resume.data.education;
+        this.tableData[i]["community"] = resume.data.community;
+        this.tableData[i]["project"] = resume.data.project;
+        this.tableData[i]["academic"] = resume.data.academic;
+        this.tableData[i]["skill"] = resume.data.skill;
+        this.tableData[i]["introduction"] = resume.data.introduction;
+        // console.log(this.tableData[i]);
       }
+      //取消加载的转圈圈
+      this.loading = false;
     }
   },
 
   async mounted() {
-    // await this.getReceivedResume();
+    await this.getReceivedResume();
+    console.log(this.tableData);
   }
 };
 </script>
