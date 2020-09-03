7cce4546ec82943bf6568dbc53409d46c0b5c3de
chuckleless <chuckle@tongji.edu.n>
2020-09-03 12:14:07 +0800

    update announce

--- backend/Controllers/AnnounceController.cs
+++ backend/Controllers/AnnounceController.cs
@@ -49,19 +49,21 @@ namespace SyaApi.Controllers
             }
             //取得存在cookie的当前账户id
             var user_id = Int32.Parse(User.Identity.Name);
-            // if (await UserAccessor.CheckRole(user_id) == Role.Admin)
-            // {
-            //     return -10;
-            // }
+            var role=await UserAccessor.CheckRole(user_id);
+            if (role == Role.Student ||role==Role.Provider )
+            {
+                return -10;
+            }
             //生成annouce实例
             var announcement = _mapper.Map<AnnounceEntity>(request);
             announcement.user_id=user_id;
             //新建annouce
             var num=await AnnounceAccessor.Create(announcement);
-           
-            return user_id;
+            var s= await AnnounceAccessor.Createsend(num);
+            return num;
         }
 
+        //获取用户未删除的所有公告
         [HttpPost("GetAnnounce")]
         [AllowAnonymous]
         public async Task<ActionResult<AnnounceItemResponse>> GetAnnounce([FromBody]ViewAnnounceRequest request)
@@ -74,7 +76,10 @@ namespace SyaApi.Controllers
             var start=(request.pagenum-1)*request.pagesize;
             var end=request.pagenum*request.pagesize-1;
 
-            var temp=await AnnounceAccessor.Find();
+            //取得存在cookie的当前账户id
+            var user_id = Int32.Parse(User.Identity.Name);
+
+            var temp=await AnnounceAccessor.Find(user_id);
             for(int i=0;i<temp.totalpage;i++)
             {                    
                 ans.totalpage++;
@@ -87,21 +92,24 @@ namespace SyaApi.Controllers
             return Ok(ans);
         }
 
-
-        [HttpPost("ViewedAnnounce")]
+        //查看公告详细信息
+        [HttpPost("GetAnnounceContent")]
         [AllowAnonymous]
-        public async Task<int> ViewedAnnounce([FromBody] AnnounceStatusRequest request)
+        public async Task<AnnounceResponse> GetAnnounceContent([FromBody] AnnounceStatusRequest request)
         {
             //取得存在cookie的当前账户id
             var temp_id = Int32.Parse(User.Identity.Name);
 
+            var ans=await AnnounceAccessor.FindInfo(request.announcement_id);
+            
             var temp_entity = _mapper.Map<AnnounceEntity>(request);
+            temp_entity.user_id=temp_id;
             var temp =await AnnounceAccessor.Change(temp_entity);
-
-            return temp;
+            ans.status=1;
+            return _mapper.Map<AnnounceResponse>(ans);
         }
 
-
+        //非管理员删除公告
         [HttpPost("DeleteAnnounce")]
         [AllowAnonymous]
         public async Task<int> DeleteAnnounce([FromBody] AnnounceStatusRequest request)
@@ -110,9 +118,30 @@ namespace SyaApi.Controllers
             var temp_id = Int32.Parse(User.Identity.Name);
 
             var temp_entity = _mapper.Map<AnnounceEntity>(request);
+            temp_entity.user_id=temp_id;
             var temp =await AnnounceAccessor.Delete(temp_entity);
 
             return temp;
         }
+    
+        //管理员删除公告
+        [HttpPost("DeleteAnnounceAll")]
+        [AllowAnonymous]
+        public async Task<int> DeleteAnnounceAll([FromBody] AnnounceStatusRequest request)
+        {
+            //取得存在cookie的当前账户id
+            var temp_id = Int32.Parse(User.Identity.Name);
+            var role=await UserAccessor.CheckRole(temp_id);
+            if (role == Role.Student ||role==Role.Provider )
+            {
+                return -10;
+            }
+            var temp_entity = _mapper.Map<AnnounceEntity>(request);
+            var temp =await AnnounceAccessor.DeleteAll(temp_entity);
+
+            return temp;
+        }
+    
+    
     }
 }
