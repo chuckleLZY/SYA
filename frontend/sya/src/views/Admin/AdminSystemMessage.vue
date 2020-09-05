a6f6fd59f213ab98e6c4759815a586a973786c3b
Cffuu <1289079621@qq.com>
2020-09-05 16:39:33 +0800

    update router

--- frontend/sya/src/views/Admin/AdminSystemMessage.vue
+++ frontend/sya/src/views/Admin/AdminSystemMessage.vue
@@ -1,9 +1,8 @@
 <template>
-    <div>
-        这里是管理员系统通知 --已作废
+  <div>
+    这里是管理员系统通知 --已作废
     <el-card class="student_card">
-            
-           <!-- <el-row>
+      <!-- <el-row>
                 <el-col :span="7">
                     <el-input placeholder="请输入内容" v-model="queryInfo.query" class="input-with-select">
                         
@@ -11,103 +10,92 @@
                     </el-input>
                 </el-col>
                 <el-col :span="4"></el-col>
-            </el-row>-->
-            
-            <!-- 列表区域 -->
-            <el-table :data="sysMessageList" :row-class-name="tableRowClassName" v-loading="loading">
-                <el-table-column label="#" type="index"></el-table-column>
-                <el-table-column
-                    prop="title"
-                    label="公告标题"
-                    width="280">
-                </el-table-column>
-                <el-table-column
-                    prop="send_time"
-                    label="发送时间"
-                    width="280">
-                </el-table-column>
-                <el-table-column
-                    prop="status"
-                    label="状态"
-                    width="280">
-                </el-table-column>
-               <!-- <el-table-column
+      </el-row>-->
+
+      <!-- 列表区域 -->
+      <el-table :data="sysMessageList" :row-class-name="tableRowClassName" v-loading="loading">
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column prop="title" label="公告标题" width="280"></el-table-column>
+        <el-table-column prop="send_time" label="发送时间" width="280"></el-table-column>
+        <el-table-column prop="status" label="状态" width="280"></el-table-column>
+        <!-- <el-table-column
                     prop="address"
                     label="地址">
-                </el-table-column>-->
-                <el-table-column label="操作">
-                    <template v-slot:default="scope">
-                       
+        </el-table-column>-->
+        <el-table-column label="操作">
+          <template v-slot:default="scope">
+            <el-tooltip effect="dark" content="查看详情" placement="top-start" :enterable="false">
+              <el-button
+                type="primary"
+                icon="el-icon-edit"
+                size="mini"
+                @click="viewMesInfo(scope.row)"
+                circle
+              ></el-button>
+            </el-tooltip>
+            <el-tooltip effect="dark" content="删除" placement="top-start" :enterable="false">
+              <el-button
+                type="primary"
+                icon="el-icon-delete"
+                size="mini"
+                @click="removeMesById(scope.row.announcement_id)"
+                circle
+              ></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
 
-                    
-                            <el-tooltip  effect="dark" content="查看详情" placement="top-start" :enterable="false">
-                                <el-button type="primary" icon="el-icon-edit" size="mini" @click="viewMesInfo(scope.row)" circle></el-button>
-                            </el-tooltip>
-                            <el-tooltip  effect="dark" content="删除" placement="top-start" :enterable="false">
-                                <el-button type="primary" icon="el-icon-delete" size="mini" @click="removeMesById(scope.row.announcement_id)" circle></el-button>
-                            </el-tooltip>
-                    </template>
+      <!--  分页区域  -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-card>
 
-                            
-                </el-table-column> 
-
-            </el-table>
-
-            <!--  分页区域  -->
-            <el-pagination
-                @size-change="handleSizeChange"
-                @current-change="handleCurrentChange"
-                :current-page="queryInfo.pagenum"
-                :page-sizes="[1, 2, 5, 10]"
-                :page-size="queryInfo.pagesize"
-                layout="total, sizes, prev, pager, next, jumper"
-                :total="total">
-            </el-pagination>
-        </el-card>
-
-        <el-dialog title="详情" :visible.sync="checkDialogVisible" width="35%">
+    <el-dialog title="详情" :visible.sync="checkDialogVisible" width="35%">
       <div style="width:400px;margin:auto;">
         <el-form status-icon label-width="auto" :model="messageData">
-          
           <el-form-item label="内容">
-            <el-input  disabled v-model="messageData.content"></el-input>
+            <el-input disabled v-model="messageData.content"></el-input>
           </el-form-item>
           <el-form-item label="发送时间">
-            <el-input  disabled v-model="messageData.send_time"></el-input>
+            <el-input disabled v-model="messageData.send_time"></el-input>
           </el-form-item>
-          
-          
         </el-form>
-        
       </div>
     </el-dialog>
-
-    </div>
+  </div>
 </template>
 
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    data() {
+  data() {
     return {
-      input1: '',
-      input2: '',
-      input3: '',
-      select: '',
+      input1: "",
+      input2: "",
+      input3: "",
+      select: "",
       checkDialogVisible: false,
-      messageData:{},
+      messageData: {},
       loading: true,
-    //获取工作列表的参数对象
-      queryInfo:{
-          query: '',
-          pagenum: 1,
-          pagesize: 5
+      //获取工作列表的参数对象
+      queryInfo: {
+        query: "",
+        pagenum: 1,
+        pagesize: 5
       },
-      sysMessageList:[],
-      total:0,
+      sysMessageList: [],
+      total: 0
 
-     /* workList: [{
+      /* workList: [{
             date: '2016-05-02',
             name: '王小虎',
             address: '上海市普陀区金沙江路 1518 弄'
@@ -124,14 +112,14 @@ export default {
             name: '王小虎',
             address: '上海市普陀区金沙江路 1516 弄'
           }]*/
-    }
+    };
   },
-  created(){
-      //调用获取发布的工作的API函数
-      this.getSysMessageList()
+  created() {
+    //调用获取发布的工作的API函数
+    this.getSysMessageList();
   },
-  methods:{
-    tableRowClassName({ row,rowIndex }) {
+  methods: {
+    tableRowClassName({ row, rowIndex }) {
       // console.log(row);
       if (row.status === 0) {
         return "havenot-row";
@@ -140,25 +128,25 @@ export default {
       }
       return "";
     },
-      //监听每页条数选项改变的事件
-      handleSizeChange(newSize){
-        //console.log(newSize)
-        this.loading = true;
-        this.queryInfo.pagesize=newSize
-        this.getSysMessageList()
-        this.loading = false;
-      },
-      //监听页码改变的事件
-      handleCurrentChange(newPage){
-       // console.log(newPage)
-       this.loading = true;
-        this.queryInfo.pagenum=newPage
-        this.getSysMessageList()
-        this.loading = false;
-      },
-  //获取工作列表的函数
-   async getSysMessageList(){
-         const res = await axios.post(
+    //监听每页条数选项改变的事件
+    handleSizeChange(newSize) {
+      //console.log(newSize)
+      this.loading = true;
+      this.queryInfo.pagesize = newSize;
+      this.getSysMessageList();
+      this.loading = false;
+    },
+    //监听页码改变的事件
+    handleCurrentChange(newPage) {
+      // console.log(newPage)
+      this.loading = true;
+      this.queryInfo.pagenum = newPage;
+      this.getSysMessageList();
+      this.loading = false;
+    },
+    //获取工作列表的函数
+    async getSysMessageList() {
+      const res = await axios.post(
         "http://localhost:5000/Announce/GetAnnounce",
         {
           pagenum: this.queryInfo.pagenum,
@@ -171,31 +159,34 @@ export default {
       if (res.status !== 200) {
         this.$message.error("Unexpected response");
         return;
-        }
-        this.sysMessageList=res.data.announceItem;
-        this.total=res.data.totalpage;
-        this.pagesize=res.data.totalpage/res.data.pagenum;
-        this.pagenum=res.data.pagenum;
-        this.loading = false;
-        //console.log(this.pagesize);
-        console.log(this.sysMessageList);
-      },
+      }
+      this.sysMessageList = res.data.announceItem;
+      this.total = res.data.totalpage;
+      this.pagesize = res.data.totalpage / res.data.pagenum;
+      this.pagenum = res.data.pagenum;
+      this.loading = false;
+      //console.log(this.pagesize);
+      console.log(this.sysMessageList);
+    },
 
-      async removeMesById(id){
-        const confirmResulte=await this.$confirm('此操作将永久删除该工作消息, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).catch(err=>err)
-        //console.log(confirmResulte);
-        if(confirmResulte!=='confirm'){
-          return this.$message.info("已取消删除");
+    async removeMesById(id) {
+      const confirmResulte = await this.$confirm(
+        "此操作将永久删除该工作消息, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
         }
-        const res = await axios.post(
+      ).catch(err => err);
+      //console.log(confirmResulte);
+      if (confirmResulte !== "confirm") {
+        return this.$message.info("已取消删除");
+      }
+      const res = await axios.post(
         "http://localhost:5000/Announce/DeleteAnnounce",
         {
           announcement_id: id
-          
         },
         {
           withCredentials: true
@@ -205,16 +196,15 @@ export default {
       if (res.status !== 200) {
         this.$message.error("Unexpected response");
         return;
-        }
+      }
       this.$message.info("已成功删除");
       this.getSysMessageList();
+    },
 
-      },
-
-      async viewMesInfo(row){
-        this.messageData = row;
-        this.checkDialogVisible = true;
-        const res = await axios.post(
+    async viewMesInfo(row) {
+      this.messageData = row;
+      this.checkDialogVisible = true;
+      const res = await axios.post(
         "http://localhost:5000/Announce/GetAnnounceContent",
         {
           announcement_id: row.announcement_id
@@ -223,19 +213,17 @@ export default {
           withCredentials: true
         }
       );
-      }
+    }
   }
-}
+};
 </script>
 
 
 <style scoped>
-
 </style>
 
 
 <style>
-
 .el-table .havenot-row {
   background: oldlace;
 }
