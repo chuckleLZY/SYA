ac57acc43b84131029c7baeaf5256564091881ca
ComposeC <49675235+ComposeC@users.noreply.github.com>
2020-09-08 18:42:04 +0800

    创建工作时间设置
    (1)work表增加start_day, end_day,
    tart_time, end_time,
    week_day, total_time
    (2)计算工作总小时数的函数CalTotalTime()
    (3)更新api: CreateWork, ViewHistoryWork

--- backend/Entities/WorkEntity.cs
+++ backend/Entities/WorkEntity.cs
@@ -16,6 +16,12 @@ namespace SyaApi.Entities
         public int likes_num{get; set;}
         public int collect_num{get; set;}
         public int share_num{get; set;}
+        public string start_day{get; set;}
+        public string end_day{get; set;}
+        public string start_time{get; set;}
+        public string end_time{get; set;}
+        public double total_time{get; set;}
+        public int week_day{get; set;}
     }
 
 
