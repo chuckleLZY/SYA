bd8e4394982ca68ccb25b81ac9eb1f6ec87abce3
Cffuu <1289079621@qq.com>
2020-09-12 08:56:48 +0800

    ui

---
+++ frontend/sya/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-footer style="padding-left: 0px;padding-right: 0px;height: 100px">
+      <img src="../assets/imagebox/footlogo.png" />
+    </el-footer>
+  </div>
+</template>
+
+
+<style scoped>
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  /* line-height: 60px; */
+}
+</style>
+
