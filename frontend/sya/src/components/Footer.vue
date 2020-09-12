580c95cf5ac233f800e38f76c5d2c87e563e66ca
Cffuu <1289079621@qq.com>
2020-09-12 20:46:40 +0800

    footer

--- frontend/sya/src/components/Footer.vue
+++ frontend/sya/src/components/Footer.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <el-footer style="padding-left: 0px;padding-right: 0px;height: 100px">
-      <img src="../assets/imagebox/footlogo.png" />
+    <el-footer style="padding-left: 0px;padding-right: 0px;height: 100px;">
+      <div style="position:relative;padding-top:13px;text-align:center">
+        <img src="../assets/imagebox/footlogo.png" />
+        <div
+          style="position:absolute;z-indent:9;display: inline-block;width:500px;margin-left:-362px;padding-top:8px"
+        >
+          <div style="font-weight:bolder"> SYA——Show Your Ability</div>
+          <br />联系我们： 
+          <i class="el-icon-s-custom"></i>同济大学软件学院  
+          <i class="el-icon-position"></i>上海市嘉定区曹安公路4800号
+        </div>
+      </div>
     </el-footer>
   </div>
 </template>
