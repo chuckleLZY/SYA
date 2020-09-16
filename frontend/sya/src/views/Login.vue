a151c88323b9d0592f7bb0a737c0075cafd852a3
Kagurazaka Kotori <kagurazakakotori@gmail.com>
2020-09-16 11:30:52 +0800

    frontend/Login.vue: remove unused functions

--- frontend/sya/src/views/Login.vue
+++ frontend/sya/src/views/Login.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="login_container">
-<el-carousel :interval="4000" type="card" height="200px">
+    <el-carousel :interval="4000" type="card" height="200px">
       <el-carousel-item v-for="item in imagebox" :key="item.id">
         <img :src="item.idView" class="image" />
       </el-carousel-item>
     </el-carousel>
-  <el-divider><tr style="font-size : 30px;">
-    <td class="color-dark-light">登录</td>
-    </tr></el-divider>
+    <el-divider>
+      <tr style="font-size : 30px;">
+        <td class="color-dark-light">登录</td>
+      </tr>
+    </el-divider>
     <el-divider direction="vertical"></el-divider>
     <br />
     <el-divider direction="vertical"></el-divider>
@@ -47,14 +49,17 @@
     <el-divider direction="vertical"></el-divider>
     <br />
     <el-divider direction="vertical"></el-divider>
-    <br>
+    <br />
     <el-divider direction="vertical"></el-divider>
-    <br>
+    <br />
     <el-divider direction="vertical"></el-divider>
-    <br>
+    <br />
     <el-divider direction="vertical"></el-divider>
-    <br>
-    <span style="font-size: 12px">登录代表您同意我们的</span><el-link style="font-size:12px" type="primary">隐私政策</el-link><span style="font-size: 12px">和</span><el-link style="font-size:12px" type="primary">用户协议</el-link>
+    <br />
+    <span style="font-size: 12px">登录代表您同意我们的</span>
+    <el-link style="font-size:12px" type="primary">隐私政策</el-link>
+    <span style="font-size: 12px">和</span>
+    <el-link style="font-size:12px" type="primary">用户协议</el-link>
     <div class="verticalBar"></div>
     <div class="erweima_box">
       <b>满意的话，请给我们项目一点支持</b>
@@ -66,7 +71,7 @@
     <div class="login_box">
       <!-- <div class="avatar_box">
         <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1599575737996&di=22f8c24ab5079385e3ff45d8576179c0&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201809%2F15%2F20180915192557_JSFLK.jpeg" alt />
-      </div> -->
+      </div>-->
       <el-form ref="loginFormRef" label-width="0px" class="login_form" :model="logInForm" :rules="loginFormRules">
         <!-- 用户名 -->
         <el-form-item prop="username">
@@ -79,8 +84,8 @@
         <!-- 按钮 -->
         <el-form-item class="btns">
           <el-button type="primary" @click="login()">登录</el-button>
-          <el-button type="info" @click="register()">注册</el-button>
-          <el-button  @click="resetLoginForm">重置</el-button>
+          <el-button type="info" @click="$router.push('/Register')">注册</el-button>
+          <el-button @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -99,20 +104,20 @@ export default {
       //登录表单
       logInForm: {
         username: "",
-        Password: ""
+        Password: "",
       },
       loginFormRules: {
         username: [
           { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" },
         ],
         Password: [
           { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
-        ]
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" },
+        ],
       },
       imagebox: [
-         { id: 0, idView: require("../assets/imagebox/login1.png") },
+        { id: 0, idView: require("../assets/imagebox/login1.png") },
         { id: 1, idView: require("../assets/imagebox/login2.png") },
         { id: 2, idView: require("../assets/imagebox/login3.png") },
         { id: 3, idView: require("../assets/imagebox/login4.png") },
@@ -122,64 +127,61 @@ export default {
       // 图片地址数组
       imgList: [
         {
-          url: require("@/assets/logo.png")
+          url: require("@/assets/logo.png"),
         },
         {
-          url: require("@/assets/logo.png")
+          url: require("@/assets/logo.png"),
         },
         {
-          url: require("@/assets/logo.png")
-        }
+          url: require("@/assets/logo.png"),
+        },
       ],
       //图片父容器高度
       bannerHeight: 1000,
       //浏览器宽度
-      screenWidth: 0
+      screenWidth: 0,
     };
   },
   methods: {
-     resetLoginForm () {
+    resetLoginForm() {
       // console.log(this)
       // resetFields：element-ui提供的表单方法
-      this.$refs.loginFormRef.resetFields()
+      this.$refs.loginFormRef.resetFields();
     },
     async login() {
       console.log(this.logInForm);
-     try{ const result = await axios.post(
-        "http://localhost:5000/Account/Login",
-        {
-          username: this.logInForm.username,
-          // 密码哈希
-          Password: sha512(this.logInForm.Password).toString()
-        },
-        { withCredentials: true }
-      );
-      console.log(result);
-      if (result.status == 200) {
-        //将登录信息保存到vuex
-        this.$store.commit("logIn", result.data.role);
-        // //将vuex里的信息保存到sessionStorage里
-        // sessionStorage.setItem("store", JSON.stringify(this.$store.state));
-        this.$message({
-          message: '登录成功',
-          type: 'success',
-          duration:500
-        });
-        this.$router.push("/Main");
-      }
-     }
-      catch(err){       
+      try {
+        const result = await axios.post(
+          "http://localhost:5000/Account/Login",
+          {
+            username: this.logInForm.username,
+            // 密码哈希
+            Password: sha512(this.logInForm.Password).toString(),
+          },
+          { withCredentials: true }
+        );
+        console.log(result);
+        if (result.status == 200) {
+          //将登录信息保存到vuex
+          this.$store.commit("logIn", result.data.role);
+          // //将vuex里的信息保存到sessionStorage里
+          // sessionStorage.setItem("store", JSON.stringify(this.$store.state));
           this.$message({
-          message: '登录失败',
-          type: 'error',
-          duration:500
+            message: "登录成功",
+            type: "success",
+            duration: 500,
           });
+          this.$router.push("/Main");
         }
-      },
-         async register() {
-      this.$router.push("/Register");
+      } catch (err) {
+        this.$message({
+          message: "登录失败",
+          type: "error",
+          duration: 500,
+        });
+      }
     }
-    }
+  },
 };
 </script>
 
