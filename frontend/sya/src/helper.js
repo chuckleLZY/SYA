20fb0e67f66881ed7385d09455ec2816c360eb00
Kagurazaka Kotori <kagurazakakotori@gmail.com>
2020-09-17 22:38:30 +0800

    frontend: fix helper.js

--- frontend/sya/src/helper.js
+++ frontend/sya/src/helper.js
@@ -2,8 +2,8 @@ import Vue from "vue";
 import config from "./config.json";
 
 const helperFunctions = {
-  endpointUrl: (endpoint) => { config.API_URL + endpoint; },
-  ossFileUrl: (filename) => { config.OSS_URL + filename }
+  endpointUrl: (endpoint) => (config.API_URL + endpoint),
+  ossFileUrl: (filename) => (config.OSS_URL + filename)
 }
 
 const helper = {
